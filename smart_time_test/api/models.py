96bab8fc867961526b64e7a7a2740826312308a5
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-02-12 15:46:28 +0300

    feat: Added exams model

--- smart_time_test/api/models.py
+++ smart_time_test/api/models.py
@@ -1,4 +1,18 @@
-from django.db.models import EmailField, IntegerField, Model, TextField
+from django.db.models import (
+    CASCADE,
+    EmailField,
+    ForeignKey,
+    IntegerField,
+    Model,
+    TextField,
+)
+
+
+class Room(Model):
+    rum_number = IntegerField()
+
+    class Meta:
+        db_table = "rooms"
 
 
 class User(Model):
@@ -9,8 +23,9 @@ class User(Model):
         db_table = "users"
 
 
-class Room(Model):
-    rum_number = IntegerField()
+class Exam(Model):
+    subject_name = TextField()
+    room_id = ForeignKey(Room, on_delete=CASCADE)
 
     class Meta:
-        db_table = "rooms"
+        db_table = "exams"
