17fe977405441e60ab99deb2cba383725e683b43
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-02-12 19:10:31 +0300

    feat: Added models to admin panel

--- smart_time_test/api/admin.py
+++ smart_time_test/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from api.models import User
+from api.models import Exam, ExamRegistration, Room, Subject, User
 
+admin.site.register(Room)
 admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Exam)
+admin.site.register(ExamRegistration)
