ea812b024b25dc887aaa88ac087919ffbd318d30
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-02-13 18:25:59 +0300

    fix: Fixed tests for user registration

--- smart_time_test/api/tests.py
+++ smart_time_test/api/tests.py
@@ -1,46 +1,47 @@
+from hashlib import sha3_512
 from http import HTTPStatus
 from json import dumps
 
 from django.test import Client, TestCase
-from parameterized import parameterized
 
 from api.models import Exam, Room, Subject, User
 
 
 class RegisterTests(TestCase):
     def setUp(self):
-        User.objects.create(email="student.1@edu.hse.ru", password="12345678")
+        email = "student@edu.hse.ru"
+        password = "12345678"
+        User.objects.create(
+            email=email, password=sha3_512(password.encode()).hexdigest()
+        )
 
-    @parameterized.expand(
-        [
-            (
-                "student.1@edu.hse.ru",
-                "1234",
-                HTTPStatus.BAD_REQUEST,
-            ),
-            (
-                "student.1",
-                "12345678",
-                HTTPStatus.BAD_REQUEST,
-            ),
-            (
-                "student.1@edu.hse.ru",
-                "12345678",
-                HTTPStatus.BAD_REQUEST,
-            ),
-            (
-                "student.2@edu.hse.ru",
-                "12345678",
-                HTTPStatus.OK,
-            ),
-        ]
-    )
-    def test_insertion(self, email, password, http_status):
-        data = {"email": email, "password": password}
+    def test_bad_password(self):
+        data = {"email": "student@edu.hse.ru", "password": "1234"}
         response = Client().post(
             "/api/register/", dumps(data), "application/json"
         )
-        self.assertEqual(response.status_code, http_status)
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+    def test_bad_email(self):
+        data = {"email": "student.edu.hse.ru", "password": "12345678"}
+        response = Client().post(
+            "/api/register/", dumps(data), "application/json"
+        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+    def test_user_exists(self):
+        data = {"email": "student@edu.hse.ru", "password": "12345678"}
+        response = Client().post(
+            "/api/register/", dumps(data), "application/json"
+        )
+        self.assertEqual(response.status_code, HTTPStatus.BAD_REQUEST)
+
+    def test_registration_success(self):
+        data = {"email": "student.1@edu.hse.ru", "password": "12345678"}
+        response = Client().post(
+            "/api/register/", dumps(data), "application/json"
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 class ExamRegistrationsTests(TestCase):
