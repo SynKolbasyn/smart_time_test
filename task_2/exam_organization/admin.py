5f175d47730c777a2446104500ec8975cfb2625b
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-02-15 13:00:29 +0300

    feat: Added small part of task 2

--- task_2/exam_organization/admin.py
+++ task_2/exam_organization/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from exam_organization.models import Subject
+from exam_organization.models import Campus, Exam, Subject, SubjectUnit, Teacher
 
 
+admin.site.register(Campus)
+admin.site.register(Exam)
 admin.site.register(Subject)
+admin.site.register(SubjectUnit)
+admin.site.register(Teacher)
--- task_2/exam_organization/models.py
+++ task_2/exam_organization/models.py
@@ -1,5 +1,65 @@
-from django.db.models import Model
+from django.db.models import (
+    CASCADE, CharField, ForeignKey, IntegerField, Model, TextChoices, TextField
+)
 
 
 class Subject(Model):
-    pass
+    name = TextField(unique=True, blank=False, null=False)
+    abbreviation = TextField(unique=True, blank=False, null=False)
+    unique_id = IntegerField(unique=True, blank=False, null=False)
+
+    class Meta:
+        db_table = "subjects"
+
+
+class Campus(Model):
+    name = TextField(unique=True, blank=False, null=False)
+    abbreviation = CharField(
+        max_length=1, unique=True, blank=False, null=False
+    )
+
+    class Meta:
+        db_table = "campuses"
+
+
+class SubjectUnit(Model):
+    class GroupType(TextChoices):
+        FLOW = "П"
+        GROUP = "Г"
+        SUB_GROUP = "С"
+
+    campus = ForeignKey(Campus, CASCADE)
+    subject = ForeignKey(Subject, CASCADE)
+    group_type = CharField(
+        max_length=1, choices=GroupType, unique=False, blank=False, null=False
+    )
+    group_number = IntegerField(unique=True, blank=False, null=False)
+
+    class Meta:
+        db_table = "subject_units"
+
+
+class Teacher(Model):
+    class TeacherType(TextChoices):
+        LECTURER = "lecturer"
+        SEMINARIAN = "seminarian"
+
+    forename = TextField(unique=False, blank=False, null=False)
+    surname = TextField(unique=False, blank=False, null=False)
+    patronymic = TextField(unique=False, blank=False, null=False)
+
+    passport_number = IntegerField(unique=True, blank=False, null=False)
+
+    type = TextField(
+        choices=TeacherType, unique=False, blank=False, null=False
+    )
+
+    class Meta:
+        db_table = "teachers"
+
+
+class Exam(Model):
+    subject_unit = ForeignKey(SubjectUnit, CASCADE)
+
+    class Meta:
+        db_table = "exams"
